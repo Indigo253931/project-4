70a288b0692b59c00ec2017966b165d0f0df955f
Indigo253931 <indigo253931@gmail.com>
2016-08-27 16:27:42 -0600

    added hardcoded data to app.js

--- client/public/scripts/app.js
+++ client/public/scripts/app.js
@@ -62,3 +62,71 @@ var app = angular.module('EcoViews', ['ngRoute'])
 		$http.post('http://localhost:3000/api/products/', newProduct);
 			console.log(newProduct);
 		}
+
+		 var products = [];
+
+ var sectors = [];
+
+// ALL_PRODUCTS = 
+// Seed Data
+
+products.push([
+{
+		'sector': 'Construction',
+		'industry': 'Building Materials & Equipment',
+		'company_name': 'American limetechnology',
+		'product_name': 'Breathe Natual Fibre Insulation',
+		'comment': ['Insulation product made from premium grade hemp and flax fibrewhich create a naturally efficient thermal and acoustic insulation product for walls, floors and roofs.'],
+		'image': 'http://americanlimetechnology.com/wp-content/uploads/2012/01/Breathe-logo-100x240.png',
+	
+	},
+{
+		'sector': 'Misc',
+		'industry': ['Food & Beverage'],
+		'company_name': 'Dr. Bronners',
+		'product_name': 'Whole Kernel Virgin Coconut Oil',
+		'comment': ['Certified Fair Trade by IMO', 'Certified Kosher by OK Kosher', 
+		'Non-GMO verified by the NON-GMO Project', 'Certified Organic by the Oregon Tilth and USDA National Organic Program', 
+		'Ceritfied vegan by Vegan Action'],
+		'image': 'https://www.drbronner.com/mm5/graphics/00000002/organic-coconutoil-whole-14oz_2.jpg'
+}
+]);
+
+
+sectors.push = ([
+{
+	sector: 'Agribusiness',
+	industries: ['Crop Production & Basic Processing', 'Tobacco', 'Dairy', 
+	'Poultry & Eggs', 'Livestock', 'Agricultural Services & Products', 
+	'Food Processing & Sales', 'Forestry & Forest Products']
+}, 
+{
+	sector: 'Communications/Electronics',
+	industries: ['Printing & Publishing', 'TV/Movies/Music', 
+	'Telephone Utilities', 'Telecom Services & Equipment', 
+	'Electronics Manufacturing & Equipment', 'Internet']
+}, 
+{
+	sector: 'Construction',
+	industries: ['General Contractors', 'Home Builders', 
+	'Special Trade Contractors','Construction Services', 
+	'Architectural Services', 'Building Materials & Equipment']
+}, 
+{
+	sector: 'Energy & Natural Resources',
+	industries: ['Oil & Gas', 'Mining', 'Alternative Energy Production & Services',
+	'Electric Utilities', 'Waste Management']
+}, 
+{
+	sector: 'Misc Buisness',
+	industries: ['Buisness Associations', 'Food & Beverage', 'Beer, Wine & Liquor',
+	'Retail Sales', 'Miscellaneous Services', 'Buisness Services', 'Recreation / Live Entertainment',
+	'Casinos / Gambling', 'Lodging / Tourism', 'Chemical & Related Manufacturing', 'Steel Production', 
+	'Misc Manufacturing & Distributing', 'Textiles']
+}, 
+{
+	sector: 'Transportation',
+	industries: ['Air Transport', 'Automotive', 'Trucking',
+	'Railroads', 'Sea Transport']
+}
+]);
