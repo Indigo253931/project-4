292d9d4e8fdb5974c121134ff2bfb09c70f35309
Indigo253931 <indigo253931@gmail.com>
2016-08-30 10:12:46 -0600

    updated script tags in index.html, installed body-parser

--- client/public/scripts/app.js
+++ client/public/scripts/app.js
@@ -1,45 +1,49 @@
 // Create an Angular module
-var app = angular.module('EcoViews', ['ngRoute'])
-.config(function($routeProvider){
-$routeProvider.when('/', {
-			templateUrl: 'templates/about.html',
-		});
-});
-	// 	.when('/about', {
-	// 		templateUrl: 'templates/about.html',
-	// 	})
-	// 	.when('/signup', {
-	// 		templateUrl: 'templates/signup.html',
-	// 		controller: 'usersController'
-	// 	})
-	// 	.when('/login', {
-	// 		templateUrl: 'templates/login.html',
-	// 		controller: 'usersController'
-	// 	})
-	// 	.when('/profile', {
-	// 		templateUrl: 'templates/profile.html',
-	// 		controller: 'usersController'
-	// 	})
-	// 	.when('products', {
-	// 		templateUrl: 'templates/productIndex.html',
-	// 		controller: 'productsController'
-	// 	})
-	// 	.when('products/:id', {
-	// 		templateUrl: 'templates/productShow.html',
-	// 		controller: 'productsController'
-	// 	})
-	// 	.when('products/new', {
-	// 		templateUrl: 'templates/productNew.html',
-	// 		controller: 'productsController'
-	// 	})
-	// 	.when('sectors', {
-	// 		templateUrl: 'templates/productIndex.html',
-	// 		controller: 'sectorsController'
-	// 	})
-	// 	.when('sectors/:id', {
-	// 		templateUrl: 'templates/productShow.html',
-	// 		controller: 'sectorsController'
-	// 	});
+var app = angular.module('EcoViews', ['ngRoute']);
 
-	// });
-	
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'mainController'
+	})
+	.when('/about', {
+		templateUrl: 'templates/about.html'
+	})
+	.when('/signup', {
+		templateUrl: 'templates/signup.html',
+		controller: 'UsersController'
+	})
+	.when('/login', {
+		templateUrl: 'templates/login.html',
+		controller: 'UsersController'
+	})
+	.when('/profile', {
+		templateUrl: 'templates/profile.html',
+		controller: 'UsersController'
+	})
+	.when('products', {
+		templateUrl: 'templates/productIndex.html',
+		controller: 'ProductsController'
+	})
+	.when('products/:id', {
+		templateUrl: 'templates/productShow.html',
+		controller: 'ProductsController'
+	})
+	.when('products/', {
+		templateUrl: 'templates/productNew.html',
+		controller: 'ProductsController'
+	})
+	.when('sectors', {
+		templateUrl: 'templates/productIndex.html',
+		controller: 'SectorsController'
+	})
+	.when('sectors/:id', {
+		templateUrl: 'templates/productShow.html',
+		controller: 'sectorsController'
+	});
+});
+	app.controller('mainController', ['$scope', function($scope){
+	$scope.message = 'Welcome to EcoViews!';
+}]);
+
